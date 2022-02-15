c5a3966f8391b5c5596bd82016c99a68ed72f8d8
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-02-16 05:27:59 +0900

    fix: try another model

--- model/model.py
+++ model/model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
+import timm
 
 
 class MnistModel(BaseModel):
@@ -20,3 +21,21 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+class efficientnet_base(nn.Module):
+    def __init__(
+        self,
+        base_model = 'efficientnet_b4',
+        ouput_dim = 1000,
+        drop_p = 0.1
+        ):
+        
+        super().__init__()
+        
+        self.model = timm.create_model(base_model, pretrained=True, num_classes=ouput_dim, drop_rate=drop_p)
+        #self.model = efficientnet_b5(pretrained=True)
+        
+    def forward(self,image):
+        y =self.model(image)
+        
+        return y
