9e155a2eaee32c6dfa9e0522c92ec7ef4169b5fb
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-02-16 05:02:05 +0900

    fix: plz tpu

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,12 +1,9 @@
 import numpy as np
 import torch
-import time
 import torch_xla.core.xla_model as xm
 from torchvision.utils import make_grid
 from base import BaseTrainer
 from utils import inf_loop, MetricTracker
-
-
 class Trainer(BaseTrainer):
     """
     Trainer class
@@ -30,87 +27,73 @@ class Trainer(BaseTrainer):
         self.do_validation = self.valid_data_loader is not None
         self.lr_scheduler = lr_scheduler
         self.log_step = int(np.sqrt(config['data_loader']['args']['batch_size']))
-
         self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
         self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
-
     def _train_epoch(self, epoch):
         """
         Training logic for an epoch
-
         :param epoch: Integer, current training epoch.
         :return: A log that contains average loss and metric in this epoch.
         """
         self.model.train()
         self.train_metrics.reset()
-        start = time.time()
-        for batch_idx, batch in enumerate(self.data_loader):
+        for batch_idx, (data, target) in enumerate(self.data_loader):
+            data, target = data, target
             self.optimizer.zero_grad()
-            output = self.model(batch['image'])
-            loss = self.criterion(output, batch['label'])
+            output = self.model(data)
+            loss = self.criterion(output, target)
             loss.backward()
             xm.optimizer_step(self.optimizer)
             if self.writer is not None:
                 self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.train_metrics.update('loss', loss.item()) #tpu lazy need to call item per nstep
             for met in self.metric_ftns:
-                self.train_metrics.update(met.__name__, met(output, batch['label']))
-
+                self.train_metrics.update(met.__name__, met(output, target))
             if batch_idx % self.log_step == 0 and xm.is_master_ordinal():
-                elapsed = time.time() - start
-                self.logger.debug('Train Epoch: {} {} step_time:{:.6f}s Loss: {:.6f}'.format(
+                self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
                     epoch,
                     self._progress(batch_idx),
-                    elapsed,
                     loss.item()))
-                self.writer.add_image('input', make_grid(batch['image'].cpu(), nrow=8, normalize=True))
-
+                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
             if batch_idx == self.len_epoch:
                 break
         log = self.train_metrics.result()
-
         if self.do_validation:
             val_log = self._valid_epoch(epoch)
             log.update(**{'val_'+k : v for k, v in val_log.items()})
-
         if self.lr_scheduler is not None:
             self.lr_scheduler.step()
         return log
-
     def _valid_epoch(self, epoch):
         """
         Validate after training an epoch
-
         :param epoch: Integer, current training epoch.
         :return: A log that contains information about validation
         """
         self.model.eval()
         self.valid_metrics.reset()
         with torch.no_grad():
-            for batch_idx, batch in enumerate(self.valid_data_loader):
-                output = self.model(batch['image'])
-                loss = self.criterion(output, batch['label'])
-
+            for batch_idx, (data, target) in enumerate(self.valid_data_loader):
+                data, target = data, target
+                output = self.model(data)
+                loss = self.criterion(output, target)
                 self.valid_metrics.update('loss', loss.item())
                 for met in self.metric_ftns:
-                    self.valid_metrics.update(met.__name__, met(output, batch['label']))
-                
+                    self.valid_metrics.update(met.__name__, met(output, target))
                 if self.writer is not None:
                     self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
-                    self.writer.add_image('input', make_grid(batch['image'].cpu(), nrow=8, normalize=True))
-
+                    self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
         # add histogram of model parameters to the tensorboard
         if self.writer is not None:
             for name, p in self.model.named_parameters():
                 self.writer.add_histogram(name, p, bins='auto')
         return self.valid_metrics.result()
-
     def _progress(self, batch_idx):
         base = '[{}/{} ({:.0f}%)]'
-        '''if hasattr(self.data_loader, 'n_samples'):#iteration mode
+        if hasattr(self.data_loader, 'n_samples'):#iteration mode
             current = batch_idx * self.config['data_loader']['args']['batch_size']
             total = self.n_samples
-        else:'''
-        current = batch_idx
-        total = self.len_epoch
+        else:
+            current = batch_idx
+            total = self.len_epoch
         return base.format(current, total, 100.0 * current / total)
