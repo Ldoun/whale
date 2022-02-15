a084d3e9b3b04ee150bc77283ddfc37c627d214a
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-02-16 01:36:57 +0900

    fix: fix logging setup

--- parse_config.py
+++ parse_config.py
@@ -40,7 +40,7 @@ class ConfigParser:
         write_json(self.config, self.save_dir / 'config.json')
 
         # configure logging module
-        setup_logging(self.log_dir)
+        #setup_logging(self.log_dir)  need to call this function afte tpu spawn
         self.log_levels = {
             0: logging.WARNING,
             1: logging.INFO,
