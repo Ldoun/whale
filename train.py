1ec5e2c9a949a0fb9280bce26043c4ba9b063be2
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-02-21 17:12:05 +0900

    feat: add consine scheduler

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import data_loader.data_loader as module_data
 import model.loss as module_loss
+import model.scheduler as module_scheduler
 import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
@@ -51,7 +52,7 @@ def main(index, config):
     # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
     trainable_params = filter(lambda p: p.requires_grad, model.parameters())
     optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
-    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+    lr_scheduler = config.init_obj('lr_scheduler', module_scheduler, optimizer)
 
     #loads the training data onto each tpu
     train_dataloader = pl.MpDeviceLoader(train_dataloader,device)
