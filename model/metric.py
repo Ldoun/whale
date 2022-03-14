5704cb87cde0633a51f6f3e184be2801668efc2c
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-03-14 13:50:30 +0900

    fix: acc metric

--- model/metric.py
+++ model/metric.py
@@ -4,19 +4,19 @@ import torch
 
 def accuracy(output, target):
     with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
+        pred = torch.round(output)
         assert pred.shape[0] == len(target)
         correct = 0
-        correct += torch.sum(torch.stack([t[pred[i]] == 1 for i,t in enumerate(target)])).item()
-    return correct / len(target)
+        correct += torch.sum(pred == target).item()
+    return correct / (target.size(0) * target.size(1))
 
-
+#cannot use top_k_acc for multi label problem
 def top_k_acc(output, target, k=3):
     with torch.no_grad():
         pred = torch.topk(output, k, dim=1)[1]
         assert pred.shape[0] == len(target)
         correct = 0
         for j in range(k):
-            correct += torch.sum(torch.stack([t[pred[:,j,i]] == 1 for i,t in enumerate(target)])).item()
-    return correct / len(target)
+            correct += torch.sum(pred[:, j] == target).item()
+    return correct / (target.size(0) * target.size(1))
 
