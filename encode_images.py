d2523f6fd393609c1390b924d33564d7b541da59
Ldoun <67096173+Ldoun@users.noreply.github.com>
2022-03-20 01:20:05 +0900

    fix: csv saving method

--- encode_images.py
+++ encode_images.py
@@ -8,13 +8,14 @@ from PIL import Image
 import albumentations as A
 from torch.utils.data import DataLoader
 from albumentations.pytorch import ToTensorV2
-import torch_xla.distributed.parallel_loader as pl
 from model.model import ClipImageEncoer
 from data_loader.data_loader import single_image_dataloader
 
 try:
     import torch_xla
     import torch_xla.core.xla_model as xm
+    import torch_xla.distributed.parallel_loader as pl
+    import torch_xla.distributed.xla_multiprocessing as xmp
     device = xm.xla_device()
 
 except Exception as e:
@@ -75,7 +76,7 @@ def encode_images(index, config):
             data['image'] = pd.Series(image_names)
             data['npy'] = pd.Series(np_file_names)
             data['id'] = pd.Series(ids)
-            data.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
+            data.to_csv(f'{index}_npy_image.csv', mode='a', header=not os.path.exists(f'{index}_npy_image.csv'))
 
         
     
@@ -93,63 +94,54 @@ if __name__ == '__main__':
 
     config = args.parse_args()
 
-
-    
-
     if config.use_xla:
-        encode
-        valid_sampler = torch.utils.data.distributed.DistributedSampler(
-            valid_dataset,
-            num_replicas=xm.xrt_world_size(),
-            rank=xm.get_ordinal(),
-            shuffle=False
+        xmp.spawn(encode_images, args=(config,), nprocs=8)
+    
+    else:
+        train_data = pd.read_csv(config.csv_file)
+        image_names = []
+        np_file_names = []
+        ids = []
+        data = pd.DataFrame()
+
+        save_every = config.save_freq
+
+        transforms = A.Compose([
+            A.Resize(448, 448),
+            A.Normalize(
+                    mean=[0.485, 0.456, 0.406], 
+                    std=[0.229, 0.224, 0.225], 
+                    max_pixel_value=255.0, 
+                    p=1.0
+                ),
+            ToTensorV2()], p=1.
         )
 
-        
+        model = ClipImageEncoer(
+            embed_dim= 768,
+            image_resolution= 448,
+            vision_layers= [2,3,4,2],
+            vision_width= 64,
+            vision_patch_size= None
+        ).to(device)
 
-    image_names = []
-    np_file_names = []
-    ids = []
-    data = pd.DataFrame()
-
-save_every = config.save_freq
-
-transforms = A.Compose([
-    A.Resize(448, 448),
-    A.Normalize(
-            mean=[0.485, 0.456, 0.406], 
-            std=[0.229, 0.224, 0.225], 
-            max_pixel_value=255.0, 
-            p=1.0
-        ),
-    ToTensorV2()], p=1.
-)
-
-model = ClipImageEncoer(
-      embed_dim= 768,
-      image_resolution= 448,
-      vision_layers= [2,3,4,2],
-      vision_width= 64,
-      vision_patch_size= None
-  ).to(device)
-
-with torch.no_grad():
-    for i, row in train_data.iterrows():
-        image = np.array(Image.open(os.path.join(config.image_path,row['image'])).convert('RGB'))
-        image = transforms(image=image)["image"].to(device).unsqueeze(0)
-        
-        image_feature, logit_scale = model.encode_image(image)
-        np_image_feature = image_feature.cpu().numpy()
-        
-        np.save(os.path.join(config.save_path, f'{i}.npy'), np_image_feature)
-        image_names.append(row['image'])
-        np_file_names.append(f'{i}.npy')
-        ids.append(row['individual_id'])
-        
-        if i % save_every == 0:
-            print(f'{i}/{len(train_data)}',flush=True)
-            print(logit_scale,flush=True)
-            data['image'] = pd.Series(image_names)
-            data['npy'] = pd.Series(np_file_names)
-            data['id'] = pd.Series(ids)
-            data.to_csv('encode_image.csv',index=False)
+        with torch.no_grad():
+            for i, row in train_data.iterrows():
+                image = np.array(Image.open(os.path.join(config.image_path,row['image'])).convert('RGB'))
+                image = transforms(image=image)["image"].to(device).unsqueeze(0)
+                
+                image_feature, logit_scale = model.encode_image(image)
+                np_image_feature = image_feature.cpu().numpy()
+                
+                np.save(os.path.join(config.save_path, f'{i}.npy'), np_image_feature)
+                image_names.append(row['image'])
+                np_file_names.append(f'{i}.npy')
+                ids.append(row['individual_id'])
+                
+                if i % save_every == 0:
+                    print(f'{i}/{len(train_data)}',flush=True)
+                    print(logit_scale,flush=True)
+                    data['image'] = pd.Series(image_names)
+                    data['npy'] = pd.Series(np_file_names)
+                    data['id'] = pd.Series(ids)
+                    data.to_csv('encode_image.csv',index=False)
